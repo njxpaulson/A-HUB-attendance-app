431991fd84b0a5afc5c97b5a7c7d94a7f2d9eeaf
Nathan <nathanpaulson0@gmail.com>
2025-03-07 11:42:17 +0100

    updated config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 // config.js
 const API_BASE_URL = window.location.hostname === '127.0.0.1'
   ? 'http://localhost:3000' // Development
-  : 'https://api.yourdomain.com'; // Production
+  : 'https://a-hub-attendance-app.onrender.com'; // Production
 
 export { API_BASE_URL };
